b88aa9894f06d3190a8ddaf93c7aec6a9aea2d9f
裕二 菅野 <190154@jc-21.jp>
2022-02-08 12:55:02 +0900

    プロジェクト ファイルを追加します。

---
+++ DirectedE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject001
+{
+    public class DirectedE
+    {
+        public static int Search(int[] f, int[] n, int a)
+        {
+            return 10;
+        }
+    }
+}
